9b3c36978ec5551e68f40cc8355aae42d10d10d9
Alwaez18 <tuktukin@bk.ru>
2026-01-26 19:54:41 +0300

    Добавлен Python-скрипт: игра с угадыванием возраста

---
+++ myscript.py
@@ -0,0 +1,17 @@
+print("Hello, What is your Name?")
+name = input()
+print('Приятно познакомиться, ', name, '!', sep ='')
+age = int(input('Сколько тебе лет, '+ name + '?'))
+print('А я думал тебе ', age + 1, end = '')
+x = age + 1
+if x >=11 and x<=19:
+    print('лет')
+elif x % 10==1:
+    print('год')
+elif x % 10 >= 2 and x % 10<=4:
+    print('года')
+else:
+    print('лет')
+
+print('!..')
+
